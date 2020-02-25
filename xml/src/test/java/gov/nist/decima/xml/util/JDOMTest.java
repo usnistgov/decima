c1c9504854410d640a9443e9252427b257777ec3
David Waltermire <david.waltermire@nist.gov>
2020-02-24 22:11:56 -0500

    Added boilerplate markdown files and issue templates. Reformatted source. Fixed a bug causing Decima to not work on Java versions greater than 1.8.

--- xml/src/test/java/gov/nist/decima/xml/util/JDOMTest.java
+++ xml/src/test/java/gov/nist/decima/xml/util/JDOMTest.java
@@ -1,27 +1,32 @@
 /**
  * Portions of this software was developed by employees of the National Institute
- * of Standards and Technology (NIST), an agency of the Federal Government.
- * Pursuant to title 17 United States Code Section 105, works of NIST employees are
- * not subject to copyright protection in the United States and are considered to
- * be in the public domain. Permission to freely use, copy, modify, and distribute
- * this software and its documentation without fee is hereby granted, provided that
- * this notice and disclaimer of warranty appears in all copies.
+ * of Standards and Technology (NIST), an agency of the Federal Government and is
+ * being made available as a public service. Pursuant to title 17 United States
+ * Code Section 105, works of NIST employees are not subject to copyright
+ * protection in the United States. This software may be subject to foreign
+ * copyright. Permission in the United States and in foreign countries, to the
+ * extent that NIST may hold copyright, to use, copy, modify, create derivative
+ * works, and distribute this software and its documentation without fee is hereby
+ * granted on a non-exclusive basis, provided that this notice and disclaimer
+ * of warranty appears in all copies.
  *
  * THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, EITHER
  * EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY
  * THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND FREEDOM FROM
  * INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION WILL CONFORM TO THE
- * SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE. IN NO EVENT
+ * SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE ERROR FREE.  IN NO EVENT
  * SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT,
- * INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR
- * IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY,
+ * INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM,
+ * OR IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY,
  * CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY PERSONS OR
  * PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT
  * OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
  */
 
 package gov.nist.decima.xml.util;
+
+import gov.nist.decima.core.classpath.ClasspathHandler;
 
 import org.apache.xerces.util.XMLCatalogResolver;
 import org.jdom2.Document;
@@ -33,6 +38,7 @@ import org.jdom2.input.sax.XMLReaderJDOMFactory;
 import org.jdom2.input.sax.XMLReaderSchemaFactory;
 import org.jdom2.input.sax.XMLReaders;
 import org.jdom2.located.LocatedJDOMFactory;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.ls.LSInput;
 import org.w3c.dom.ls.LSResourceResolver;
@@ -61,6 +67,11 @@ public class JDOMTest {
 
   private static final File XSI_FILE = new File("src/test/resources/jdom-resource-resolution/with-xsi.xml");
   private static final File NO_XSI_FILE = new File("src/test/resources/jdom-resource-resolution/no-xsi.xml");
+
+  @BeforeClass
+  public static void initialize() {
+    ClasspathHandler.initialize();
+  }
 
   @Test
   public void testEntityResolver() throws JDOMException, IOException {
